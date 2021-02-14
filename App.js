4fb015a82900418497049151948f997982ea895b
tarrinneal <tarrinneal@gmail.com>
2021-02-13 20:34:13 -0800

    added stuff

---
+++ App.js
@@ -0,0 +1,11 @@
+mport React, { Component } from 'react';
+class App extends Component{
+   render(){
+      return(
+         <div>
+            <h1>Hello World</h1>
+         </div>
+      );
+   }
+}
+export default App;
